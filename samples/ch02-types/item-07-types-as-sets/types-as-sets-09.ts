a5b062e65dc1da7e843d0851cf08a8fe31b2c9bb
jasonlotz <jasonlotz@gmail.com>
2023-11-15 09:18:25 -0500

    Updates for item #7.

--- samples/ch02-types/item-07-types-as-sets/types-as-sets-09.ts
+++ samples/ch02-types/item-07-types-as-sets/types-as-sets-09.ts
@@ -10,7 +10,13 @@ interface Lifespan {
 }
 type PersonSpan = Person & Lifespan;
 const ps: PersonSpan = {
-  name: 'Alan Turing',
-  birth: new Date('1912/06/23'),
-  death: new Date('1954/06/07'),
-};  // OK
+  name: "Alan Turing",
+  birth: new Date("1912/06/23"),
+  death: new Date("1954/06/07"),
+}; // OK
+
+type PersonSpan2 = keyof (Person & Lifespan); // "name" | "birth" | "death?"
+type PersonSpan3 = keyof Person | keyof Lifespan; // "name" | "birth" | "death?"
+
+type PersonSpan4 = keyof (Person | Lifespan); // never
+type PersonSpan5 = keyof Person & keyof Lifespan; // never
